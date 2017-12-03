bf82336fc10c06bb9fb56f950d1b563057f16501
phirwe <phirwe@gmail.com>
2017-12-03 15:28:40 -0700

    Login
    Login model added

---
+++ MVCSpring/src/main/java/models/Login.java
@@ -0,0 +1,29 @@
+package models;
+
+public class Login {
+	private Integer login_id;
+	private String password;
+	private String type;
+	
+	public Integer getLogin_id() {
+		return login_id;
+	}
+	public void setLogin_id(Integer login_id) {
+		this.login_id = login_id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+	
+}
