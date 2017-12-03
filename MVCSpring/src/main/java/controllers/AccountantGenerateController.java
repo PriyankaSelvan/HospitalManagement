14fa4700ab8ceac8a937cc8d5aa8fac925a8b1bd
PriyaSelvan <selvapriyaselvan@gmail.com>
2017-12-03 16:24:40 -0700

    Prescription and billing controller

---
+++ MVCSpring/src/main/java/controllers/AccountantGenerateController.java
@@ -0,0 +1,83 @@
+package controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.ModelMap;
+
+import models.Prescription;
+import models.Inventory;
+import models.Bill;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
+@Controller
+public class AccountantGenerateController {
+	
+   @RequestMapping(value = "/accountantGenerateBill", method = RequestMethod.GET)
+   public ModelAndView patient() {
+      return new ModelAndView("accountantGenerateBill", "command", new Bill());
+   }
+   
+   @RequestMapping(value = "/accountantGenerateBillPost", method = RequestMethod.POST)
+   public String addStudent(@ModelAttribute("MVCSpring")Bill patient, 
+      ModelMap model) {
+	 //creating configuration object
+		Configuration cfg=new Configuration();
+		cfg.configure("hibernate.cfg.xml");//populates the data of the configuration file
+		
+		//creating seession factory object
+		SessionFactory factory=cfg.buildSessionFactory();
+		
+		//creating session object
+		Session session=factory.openSession();
+		
+		//creating transaction object
+		Transaction t=session.beginTransaction();
+		Prescription p = null;
+		Inventory i1 = null;
+		Inventory i2 = null;
+		Inventory i3 = null;
+		Inventory i4 = null;
+		Inventory i5 = null;
+		
+		p = (Prescription)session.get(Prescription.class, patient.getPrescription_id());
+		i1 = (Inventory)session.get(Inventory.class, p.getInventory_id_1());
+		i2 = (Inventory)session.get(Inventory.class, p.getInventory_id_2());
+		i3 = (Inventory)session.get(Inventory.class, p.getInventory_id_3());
+		i4 = (Inventory)session.get(Inventory.class, p.getInventory_id_4());
+		i5 = (Inventory)session.get(Inventory.class, p.getInventory_id_5());
+		Double inv1_cost = 0.0;
+		Double inv2_cost = 0.0;
+		Double inv3_cost = 0.0;
+		Double inv4_cost = 0.0;
+		Double inv5_cost = 0.0;
+		
+		inv1_cost = (Double)i1.getCost() ;
+		inv2_cost = (Double)i2.getCost() ;
+		inv3_cost = (Double)i3.getCost() ;
+		inv4_cost = (Double)i4.getCost() ;
+		inv5_cost = (Double)i5.getCost() ;
+		
+		
+		Double final_cost;
+		final_cost = inv1_cost+inv2_cost+inv3_cost+inv4_cost+inv5_cost;
+		patient.setFinal_cost(final_cost);
+		
+		session.persist(patient);//persisting the object
+		
+		t.commit();//transaction is commited
+		session.close();
+      model.addAttribute("bill_id", patient.getBill_id());
+      model.addAttribute("patient_id", patient.getPatient_id());
+      model.addAttribute("patient_id", patient.getPrescription_id());
+      model.addAttribute("final_cost", patient.getFinal_cost());
+      
+      
+      return "result_bill";
+   }
+}
--- MVCSpring/src/main/java/controllers/DoctorUploadController.java
+++ MVCSpring/src/main/java/controllers/DoctorUploadController.java
@@ -7,15 +7,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.ui.ModelMap;
 import models.Prescription;
+import models.Patient;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import java.util.List;
-import org.hibernate.Query;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import java.util.ArrayList;
 
 @Controller
 public class DoctorUploadController {
@@ -39,7 +35,10 @@ public class DoctorUploadController {
 		
 		//creating session object
 		Session session=factory.openSession();
-		
+		Patient p=null;
+		p = (Patient)session.get(Patient.class, press.getPatient_id());
+		press.setCurrent_temperature(p.getMost_recent_temperature());
+		press.setCurrent_pressure(p.getMost_recent_pressure());
 		//creating transaction object
 		Transaction t=session.beginTransaction();
 		
@@ -49,7 +48,13 @@ public class DoctorUploadController {
 		session.close();
       model.addAttribute("patient_id", press.getPatient_id());
       model.addAttribute("prescription_id", press.getPrescription_id());
-      model.addAttribute("inventory_id", press.getInventory_id());
+      model.addAttribute("inventory_id_1", press.getInventory_id_1());
+      model.addAttribute("inventory_id_2", press.getInventory_id_2());
+      model.addAttribute("inventory_id_3", press.getInventory_id_3());
+      model.addAttribute("inventory_id_4", press.getInventory_id_4());
+      model.addAttribute("inventory_id_5", press.getInventory_id_5());
+      model.addAttribute("current_temperature", press.getCurrent_temperature());
+      model.addAttribute("current_pressure", press.getCurrent_pressure());
       
       
       return "result_prescription";
