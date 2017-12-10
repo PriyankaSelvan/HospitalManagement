b3bace0888278ff703601d60607920ea8107e193
phirwe <phirwe@gmail.com>
2017-12-09 22:26:02 -0700

    Update LoginController.java

--- MVCSpring/src/main/java/controllers/LoginController.java
+++ MVCSpring/src/main/java/controllers/LoginController.java
@@ -27,14 +27,16 @@ public class LoginController {
    public String addStudent(@ModelAttribute("MVCSpring")Login login, 
       ModelMap model) {
 	 //creating configuration object
-		Configuration cfg=new Configuration();
+		/*Configuration cfg=new Configuration();
 		cfg.configure("hibernate.cfg.xml");//populates the data of the configuration file
 		
 		//creating session factory object
 		SessionFactory factory=cfg.buildSessionFactory();
 		
 		//creating session object
-		Session session=factory.openSession();
+		Session session=factory.openSession();*/
+	   	   Session session = HibernateUtil.getSessionFromFactory();
+
 		//login.setPassword("encrypted");
 /*		
  		Patient patient = null;
