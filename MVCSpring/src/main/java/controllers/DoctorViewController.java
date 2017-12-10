a1038d998482e37775c3f3bd6b99292275733172
Priyanka Selvan <priyanka.nessie@gmail.com>
2017-12-10 14:28:27 -0700

    Update DoctorViewController.java

--- MVCSpring/src/main/java/controllers/DoctorViewController.java
+++ MVCSpring/src/main/java/controllers/DoctorViewController.java
@@ -11,7 +11,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import controllers.HibernateUtil;
+
 
 
 @Controller
